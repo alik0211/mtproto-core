029c829a25554ae89b6cef4a8451e879a0ecd1d6
Ali Gasymov <alik0211alik@gmail.com>
2020-03-06 15:56:22 +0400

    Use special browser for CI

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome'],
+
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox'],
+      },
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
