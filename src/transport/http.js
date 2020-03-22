c68767b9cff09bcce1788cfb1188f657f602aea9
Ali Gasymov <alik0211alik@gmail.com>
2020-03-22 19:24:31 +0400

    Remove axios

--- src/transport/http.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const http = axios.create();
-delete http.defaults.headers.post['Content-Type'];
-delete http.defaults.headers.common['Accept'];
-
-module.exports = http;
