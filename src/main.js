23ddcdda4afe056f2679f66010fbf03a3e7b58f1
Ali Gasymov <alik0211alik@gmail.com>
2020-04-02 16:31:50 +0400

    Verify DH params

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const Transport = require('./transport');
 const TLSerializer = require('./tl/serializer');
 const TLDeserializer = require('./tl/deserializer');
 const {
+  bytesToHex,
   getRandomBytes,
   concatBytes,
   bytesToBigInt,
@@ -219,7 +220,44 @@ class MTProto {
     this.g = bigInt(serverDHInnerData.g);
     this.gA = bytesToBigInt(serverDHInnerData.g_a);
 
+    this.verifyDhParams(this.g, this.dhPrime, this.gA);
+
     this.generateDH();
+  }
+
+  verifyDhParams(g, dhPrime, gA) {
+    if (g.toJSNumber() !== 3) {
+      throw new Error('Server_DH_inner_data.g must be equal to 3');
+    }
+
+    if (
+      dhPrime.toString(16) !==
+      'c71caeb9c6b1c9048e6c522f70f13f73980d40238e3e21c14934d037563d930f48198a0aa7c14058229493d22530f4dbfa336f6e0ac925139543aed44cce7c3720fd51f69458705ac68cd4fe6b6b13abdc9746512969328454f18faf8c595f642477fe96bb2a941d5bcd1d4ac8cc49880708fa9b378e3c4f3a9060bee67cf9a4a4a695811051907e162753b56b0f6b410dba74d8a84b2a14b3144e0ef1284754fd17ed950d5965b4b9dd46582db1178d169c6bc465b0d6ff9ca3928fef5b9ae4e418fc15e83ebea0f87fa9ff5eed70050ded2849f47bf959d956850ce929851f0d8115f635b105ee2e4e15d04b2454bf6f4fadf034b10403119cd8e3b92fcc5b'
+    ) {
+      throw new Error('Server_DH_inner_data.dh_prime incorrect');
+    }
+
+    if (gA.compareTo(bigInt.one) <= 0) {
+      throw new Error('Server_DH_inner_data.g_a incorrect: g_a <= 1');
+    }
+
+    if (gA.compareTo(dhPrime.subtract(bigInt.one)) >= 0) {
+      throw new Error(
+        'Server_DH_inner_data.g_a incorrect: g_a >= dh_prime - 1'
+      );
+    }
+
+    const twoPow = bigInt(2).pow(2048 - 64);
+
+    if (gA.compareTo(twoPow) < 0) {
+      throw new Error('Server_DH_inner_data.g_a incorrect: g_a < 2^{2048-64}');
+    }
+
+    if (gA.compareTo(dhPrime.subtract(twoPow)) >= 0) {
+      throw new Error(
+        'Server_DH_inner_data.g_a incorrect: g_a > dh_prime - 2^{2048-64}'
+      );
+    }
   }
 
   async generateDH(retryId = 0) {
