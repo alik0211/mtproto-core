70eb5323d3e2b8589a1f3683747a5c6c836e6ca2
Ali Gasymov <alik0211alik@gmail.com>
2020-09-19 21:25:37 +0400

    Update playground

--- src/__playground/node.js
+++ src/__playground/node.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const api = require('./api');
 const { getSRPParams } = require('../../');
 const { sendCode, signIn, getPassword, checkPassword } = require('./auth');
 
@@ -17,35 +18,67 @@ function prompt(question) {
   });
 }
 
-(async () => {
-  const phone = await prompt('phone: ');
-
-  const authResult = await sendCode(phone).then(async sendCodeResult => {
-    const code = await prompt('code: ');
-    return signIn({
-      code,
-      phone,
-      phone_code_hash: sendCodeResult.phone_code_hash,
-    }).catch(async error => {
-      if (error.error_message === 'SESSION_PASSWORD_NEEDED') {
-        const password = await prompt('password: ');
-        return getPassword().then(async result => {
-          const { srp_id, current_algo, srp_B } = result;
-          const { g, p, salt1, salt2 } = current_algo;
-          const { A, M1 } = await getSRPParams({
-            g,
-            p,
-            salt1,
-            salt2,
-            gB: srp_B,
-            password,
-          });
-          return checkPassword({ srp_id, A, M1 });
-        });
-      }
-      return Promise.reject(error);
+const getUser = async () => {
+  try {
+    const user = await api.call('users.getFullUser', {
+      id: {
+        _: 'inputUserSelf',
+      },
     });
+
+    return user;
+  } catch (error) {
+    return null;
+  }
+};
+
+(async () => {
+  const user = await getUser();
+
+  if (!user) {
+    const phone = await prompt('phone: ');
+
+    const authResult = await sendCode(phone).then(async sendCodeResult => {
+      const code = await prompt('code: ');
+      return signIn({
+        code,
+        phone,
+        phone_code_hash: sendCodeResult.phone_code_hash,
+      }).catch(async error => {
+        if (error.error_message === 'SESSION_PASSWORD_NEEDED') {
+          const password = await prompt('password: ');
+          return getPassword().then(async result => {
+            const { srp_id, current_algo, srp_B } = result;
+            const { g, p, salt1, salt2 } = current_algo;
+            const { A, M1 } = await getSRPParams({
+              g,
+              p,
+              salt1,
+              salt2,
+              gB: srp_B,
+              password,
+            });
+            return checkPassword({ srp_id, A, M1 });
+          });
+        }
+        return Promise.reject(error);
+      });
+    });
+
+    console.log(`authResult:`, authResult);
+  }
+
+  // const result = await api.call('account.getAccountTTL');
+
+  // const result = await api.call('help.getConfig');
+
+  const result = await api.call('users.getUsers', {
+    id: [
+      {
+        _: 'inputUserSelf',
+      },
+    ],
   });
 
-  console.log(`authResult:`, authResult);
+  console.log(`result:`, result);
 })();
