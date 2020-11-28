3739aaea50caba2084bf685babce73efd59bd736
Ali Gasymov <alik0211alik@gmail.com>
2020-11-28 17:26:47 +0300

    5.3.0

--- src/rpc/index.js
+++ src/rpc/index.js
@@ -571,7 +571,7 @@ class RPC {
     const initConnectionParams = {
       api_id: this.api_id,
       device_model: '@mtproto/core',
-      system_version: '5.2.0',
+      system_version: '5.3.0',
       app_version: '1.0.0',
       system_lang_code: 'en',
       lang_code: 'en',
