e24fcbcf7318622dbb182a9806a603d0a8357aa4
Happy Mask Terrible Fate <happymaskterriblefate@protonmail.com>
2021-05-19 23:03:54 -0500

    Adding react-native environment.

---
+++ envs/react-native/index.js
@@ -0,0 +1,27 @@
+const { polyfillGlobal } = require("react-native/Libraries/Utilities/PolyfillFunctions");
+const { TextEncoder, TextDecoder } = require("web-encoding");
+const makeMTProto = require('../../src');
+const SHA1 = require('./sha1');
+const SHA256 = require('./sha256');
+const PBKDF2 = require('./pbkdf2');
+const Transport = require('./transport');
+const getRandomBytes = require('./get-random-bytes');
+const getLocalStorage = require('./get-local-storage');
+
+polyfillGlobal("TextEncoder", () => TextEncoder);
+polyfillGlobal("TextDecoder", () => TextDecoder);
+
+function createTransport(dc, crypto) {
+  return new Transport(dc, crypto);
+}
+
+const MTProto = makeMTProto({
+  SHA1,
+  SHA256,
+  PBKDF2,
+  getRandomBytes,
+  getLocalStorage,
+  createTransport,
+});
+
+module.exports = MTProto;
